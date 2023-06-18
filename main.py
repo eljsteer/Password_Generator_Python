b04f6039bb096f2add5c8ef6ac29f0e0d612ea3e
Jason <el.jsteer@gmail.com>
2023-06-18 12:27:45 +1000

    added in input prompts for user to enter in console

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ uppercase = [
 ]
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '@', '&', '^', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_characters = int(input("How many characters would you like in your password?\n"))
+upper_choice = input('Would you like uppercase characters? "Y" for Yes, "N" for No\n')
+numbers_choice = input('Would you like numbers? "Y" for Yes, "N" for No\n')
+symbols_choice = input('Would you like symbols? "Y" for Yes, "N" for No\n')
